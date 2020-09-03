3b00676d679acd15b3d6f1a13bda172682de1d53
Andy Shen <andyashen12@gmail.com>
2020-09-03 14:25:37 -0600

    edit

--- src/getdC.cpp
+++ src/getdC.cpp
@@ -15,17 +15,17 @@ using namespace arma;
 // }
 
 // [[Rcpp::export]]
-Rcpp::List getdC(const arma::mat& X, const arma::colvec& v, const double& s2, const double& tau2, const arma::colvec& y) {
+Rcpp::List getdC(const mat& X, const colvec& v, const double& s2, const double& tau2, const colvec& y) {
   int ncX = X.n_cols;
-  arma::mat I_ncx = eye(ncX,ncX);
-  arma::mat diagv = diagmat(v);
-  arma::mat Vinv = trans(X) * diagv * X/s2 + I_ncx/tau2;
-  arma::mat Vinvchol = chol(Vinv);
-  arma::vec dgVinvchol = diagvec(Vinvchol);
-  arma::mat V = Vinv.i(); // need chol2inv()
+  mat I_ncx = eye(ncX,ncX);
+  mat diagv = diagmat(v);
+  mat Vinv = trans(X) * diagv * X/s2 + I_ncx/tau2;
+  mat Vinvchol = chol(Vinv);
+  vec dgVinvchol = diagvec(Vinvchol);
+  mat V = Vinv.i(); // need chol2inv()
   double Vinvldet = sum(log(dgVinvchol));
-  arma::vec bhat = V * trans(X) * diagv * y/s2;
-  arma::vec d = (-0.5 * Vinvldet) + (0.5 * trans(bhat) * Vinv * bhat);
+  vec bhat = V * trans(X) * diagv * y/s2;
+  vec d = (-0.5 * Vinvldet) + (0.5 * trans(bhat) * Vinv * bhat);
   Rcpp::List L = Rcpp::List::create(d,bhat,Vinvldet,V,Vinvchol,Vinv);
   return L;
 }
